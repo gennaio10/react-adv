8aa3162325240472dfc96bcfbfb69ea47acaa893
gennaio10 <gennaio10@gmail.com>
2022-07-23 18:37:13 -0500

    con comentarios inconpletos

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -2,28 +2,31 @@ import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductArgs {
-  product: Product;
-  onChange?: (args: onChangeArgs) => void;
-  value?: number;
+    product: Product;
+    onChange?: (args: onChangeArgs) => void;
+    value?: number;
 }
 
 export function useProduct({ product, onChange, value = 0 }: useProductArgs) {
-  const [counter, setCounter] = useState(value);
+    const [counter, setCounter] = useState(value);
 
-  const increaseBy = (value: number) => {
-    //   Math.max tomara el valor mayor, en este caso si llega un negativo tomara cero
-    const newValue = Math.max(counter + value, 0);
-    setCounter(newValue);
+    // no importa si esta controlado o no :)
+    // solo se cambia en el useProduct y en el useShoppingCard
 
-    onChange && onChange({ product, count: newValue });
-  };
+    const increaseBy = (value: number) => {
+        //   Math.max tomara el valor mayor, en este caso si llega un negativo tomara cero
+        const newValue = Math.max(counter + value, 0);
+        setCounter(newValue);
 
-  useEffect(() => {
-    setCounter(value);
-  }, [value]);
+        onChange && onChange({ product, count: newValue });
+    };
 
-  return {
-    counter,
-    increaseBy,
-  };
+    useEffect(() => {
+        setCounter(value);
+    }, [value]);
+
+    return {
+        counter,
+        increaseBy,
+    };
 }
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -2,37 +2,35 @@ import { useState } from "react";
 import { Product, ProductInCart } from "../interfaces/interfaces";
 
 export function useShoppingCart() {
-  const [shoppingCart, setShoppingCart] = useState<{
-    [key: string]: ProductInCart;
-  }>({});
+    const [shoppingCart, setShoppingCart] = useState<{
+        [key: string]: ProductInCart;
+    }>({});
 
-  const onProductCountChange = ({
-    product,
-    count,
-  }: {
-    product: Product;
-    count: number;
-  }) => {
-    // console.log(count, product);
-    console.log(count);
+    const onProductCountChange = ({ product, count }: { product: Product; count: number }) => {
+        // LA ALTERNATIVA ES QUE EN EL COUNT LLEGUE 1,2,3,4 Y NO "1" o "-1"
+        // y no necesitamso saber is esta controlado o no
+        // solo se cambia en el useProduct y en el useShoppingCard
 
-    setShoppingCart((oldShoppingCard) => {
-      if (count === 0) {
-        // Mezcla entre desestructuracion, propiedades computadas y el operador rest
-        // Busca el elemento a retirar y devuelve el resto
-        const { [product.id]: toDelete, ...rest } = oldShoppingCard;
-        return rest;
-      }
+        // console.log(count, product);
+        // console.log(count);
 
-      return {
-        ...oldShoppingCard,
-        [product.id]: { ...product, count },
-      };
-    });
-  };
+        setShoppingCart((oldShoppingCard) => {
+            if (count === 0) {
+                // Mezcla entre desestructuracion, propiedades computadas y el operador rest
+                // Busca el elemento a retirar y devuelve el resto
+                const { [product.id]: toDelete, ...rest } = oldShoppingCard;
+                return rest;
+            }
 
-  return {
-    shoppingCart,
-    onProductCountChange,
-  };
+            return {
+                ...oldShoppingCard,
+                [product.id]: { ...product, count },
+            };
+        });
+    };
+
+    return {
+        shoppingCart,
+        onProductCountChange,
+    };
 }
